d000509d167f4f0670b94d655b52d1cda261ffaa
xuanhanhbn <xuanhanhbn2@gmail.com>
2023-11-14 22:43:53 +0700

    test commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Home() {
-    return <div className="text-red-500 text-lg">HOME PAGE</div>;
+    return <div className="text-red-500 text-lg">HOME PAGEs</div>;
 }
 
 export default Home;
