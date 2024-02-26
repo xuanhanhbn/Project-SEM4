92aa20c3561176ae43f7113eeb501fed6fec6fb4
duong-s-97 <duongdev97@gmail.com>
2024-02-26 21:35:06 +0700

    fix ui active token

---
+++ src/pages/user/ActiveToken/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputToken from '../Register/components/InputTocken';
+
+function ActiveTocken() {
+    return (
+        <div className="px-12 py-36 ">
+            <InputToken />
+        </div>
+    );
+}
+
+export default ActiveTocken;
