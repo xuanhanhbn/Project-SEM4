f963dc8bacd9df05d8b5bcd1eab4b3b5e3208a83
xuanhanhbn <xuanhanhbn2@gmail.com>
2024-03-24 00:13:47 +0700

    fix

--- src/pages/user/Campaigns/callApi.js
+++ src/pages/user/Campaigns/callApi.js
@@ -1,7 +1,7 @@
 import { getApiDefault } from '~/utils/api';
 
 export async function getAllPrograms() {
-    const url = '/program/get-all-programs?name=&page=0&size=0';
+    const url = '/program/get-all-programs?name=&page=1&size=20';
     try {
         const res = await getApiDefault(url);
         if (res && res?.status === 200) {
